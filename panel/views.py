d1096174360c4efd4eb8a72f168fb3f953815184
Iota <enoua5@gmail.com>
2026-07-16 20:25:42 -0700

    More layout

--- panel/views.py
+++ panel/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from panel.models import AvailableServer
+
 # Create your views here.
 
 @login_required
 def index(request) -> HttpResponse:
-    return render(request, "panel/index.html")
+    servers = AvailableServer.objects.all()
+    return render(request, "panel/index.html", { "servers": servers })

07474229d581eb60691fb445fa0161ac90a912a8
Iota <enoua5@gmail.com>
2026-07-17 23:58:55 -0700

    Command box

--- assets/scripts/custom-elements/log-list.ts
+++ assets/scripts/custom-elements/log-list.ts
@@ -1,5 +1,6 @@
 import { html, LitElement } from "lit";
 import { customElement, state } from "lit/decorators.js";
+import { ref, createRef, Ref } from "lit/directives/ref.js";
 import { ConsoleLogEvent } from "../mcsmp/mcmp.mjs";
 import { client } from "../mcsmp/mcmp-client";
 
@@ -7,6 +8,8 @@ import { client } from "../mcsmp/mcmp-client";
 class ServerLogsElement extends LitElement {
     @state()
     private events: ConsoleLogEvent[] = [];
+
+    input_ref: Ref<HTMLInputElement> = createRef();
 
     addEvent = (event: ConsoleLogEvent) => {
         this.events = [...this.events, event];
@@ -28,8 +31,33 @@ class ServerLogsElement extends LitElement {
         </div>`;
     }
 
+    async sendCommand(e: Event) {
+        e.preventDefault();
+
+        if (!this.input_ref.value) {
+            return;
+        }
+
+        const command = this.input_ref.value.value;
+        this.input_ref.value.value = "";
+
+        if (command) {
+            await client.runCommand({ command });
+        }
+    }
+
     render() {
-        return html`<div>${this.events.map(this.renderEvent)}</div>`;
+        return html`<div>
+            <div class="console-event-list">
+                ${this.events.map(this.renderEvent)}
+            </div>
+            <div class="console-controls">
+                <form @submit=${this.sendCommand}>
+                    <input ${ref(this.input_ref)} />
+                    <input type="submit" value="send" />
+                </form>
+            </div>
+        </div>`;
     }
 }
 
